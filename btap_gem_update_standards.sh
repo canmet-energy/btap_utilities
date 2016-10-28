fd023585d8c7ab8647d430cc75d9cfec0b945ea0
Phylroy Lopez <phylroy.lopez@gmail.com>
2016-10-28 18:12:16 -0400

    Update btap_gem_update_standards.sh

--- btap_gem_update_standards.sh
+++ btap_gem_update_standards.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /home/nrcan/btap_utilities \
-&& git clone https://github.com/NREL/openstudio-standards.git \
-&& cd openstudio-standards/openstudio-standards/ \
-&& git checkout migrate_necb_loads_to_standards \
-&& bundle install \
-&& rake install \
-&& cd /home/nrcan/btap_utilities
-&& rm -fr openstudio-standards
+#cd /home/nrcan/btap_utilities \
+#&& git clone https://github.com/NREL/openstudio-standards.git \
+#&& cd openstudio-standards/openstudio-standards/ \
+#&& git checkout migrate_necb_loads_to_standards \
+#&& bundle install \
+#&& rake install \
+#&& cd /home/nrcan/btap_utilities
+#&& rm -fr openstudio-standards
