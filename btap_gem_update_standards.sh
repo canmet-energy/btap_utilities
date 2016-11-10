9922020a168b34313ef0776b6f8ac53b4632bb91
Phylroy Lopez <phylroy.lopez@gmail.com>
2016-11-09 23:06:21 -0500

    change branch to nrcan

--- btap_gem_update_standards.sh
+++ btap_gem_update_standards.sh
@@ -2,7 +2,7 @@
 cd /home/nrcan/btap_utilities \
 && git clone https://github.com/NREL/openstudio-standards.git \
 && cd openstudio-standards/openstudio-standards/ \
-&& git checkout migrate_necb_loads_to_standards \
+&& git checkout nrcan \
 && /home/nrcan/.rbenv/shims/bundle install \
 && /home/nrcan/.rbenv/shims/rake install \
 && cd /home/nrcan/btap_utilities \
