17f73f3700aaf2b6f7037ae5d341c3fe25a33087
phylroy <phylroy.lopez@gmail.com>
2016-11-19 21:21:41 +0000

    stuff

--- git-phylroy.sh
+++ git-phylroy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+dropbox.py start 
+cp ~/Dropbox/linux/client_secret.json ~/.credentials/
+cp ~/Dropbox/linux/aws_config.yml ~/
+dropbox.py stop
 git config --global user.email "phylroy.lopez@gmail.com"
 git config --global user.name "phylroy"
 
