d804fb4ce6b8ddaaa3fd8115c2ba8f8b3277ab74
phylroy <phylroy.lopez@gmail.com>
2017-03-20 03:36:24 +0000

    updated

--- btap_gem_update_standards.sh
+++ btap_gem_update_standards.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
-cd ~/btap_utilities \
-&& git clone https://github.com/NREL/openstudio-standards.git \
-&& cd openstudio-standards/openstudio-standards/ \
-&& git checkout nrcan \
-&& bundle install \
-&& bundle exec rake install \
-&& cd ~/btap_utilities \
-&& rm -fr openstudio-standards
+#!/bin/bash 
+REPOSRC=https://github.com/NREL/openstudio-standards.git
+LOCALREPO=/tmp/openstudio-standards/
+
+# We do it this way so that we can abstract if from just git later on
+LOCALREPO_VC_DIR=$LOCALREPO/.git
+
+if [ ! -d $LOCALREPO_VC_DIR ]
+then
+    git clone $REPOSRC $LOCALREPO
+else
+    cd $LOCALREPO
+    git pull $REPOSRC
+fi
+cd /tmp/openstudio-standards/openstudio-standards 
+git checkout nrcan 
+/usr/local/rbenv/shims/bundle install
+/usr/local/rbenv/shims/bundle exec rake install
